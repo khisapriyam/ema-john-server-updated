4416461ba51b9f76d6fcd21e120291b3acde920d
khisapriyam <55364142+khisapriyam@users.noreply.github.com>
2020-10-14 11:34:36 +0200

    root api created

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(cors());
 
 const port = 5000
 
+app.get('/', (req, res) => {
+    res.send('hello its working')
+})
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uyzi6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
